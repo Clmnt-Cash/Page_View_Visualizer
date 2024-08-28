3749f41d3573a3e4b003d1188e6a05eeb96d4fed
Clmnt-Cash <clement.cachet@edu.ece.fr>
2024-08-28 18:52:09 +0200

    get_data

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,9 +5,12 @@ from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
 
-# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
-
+# Import data
+df = pd.read_csv("fcc-forum-pageviews.csv")
+# Set the index to the date column and parse dates
+df["date"] = pd.to_datetime(df["date"])
+df.set_index("date", inplace=True)
+print(df)
 # Clean data
 df = None
 
